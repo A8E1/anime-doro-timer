f35a4cb59db167fd36f9a88e26e6e9f17a75b218
A8E1 <98847922+A8E1@users.noreply.github.com>
2024-09-06 14:11:55 -0500

    using scanner class to get time input

---
+++ main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner timer = new Scanner(System.in);
+        System.out.println("Enter time: ");
+        int time = timer.nextInt();
+    }
+}
