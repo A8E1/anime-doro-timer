6f85afc200f970f8ddd6ae4fc10a2f5c65fe45ee
A8E1 <98847922+A8E1@users.noreply.github.com>
2024-09-06 14:49:22 -0500

    learning ScheduledExecutorService
    trying to find a way to implement a time delay so i can signal the start/end of the timer

--- main.java
+++ main.java
@@ -1,9 +1,18 @@
-import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class main {
+
+    static Runnable print = new Runnable() {
+        public void run() {
+            System.out.println("hello");
+        }
+    };
+
     public static void main(String[] args) {
-        Scanner timer = new Scanner(System.in);
-        System.out.println("Enter time: ");
-        int time = timer.nextInt();
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+        executorService.scheduleAtFixedRate(print, 0, 3, TimeUnit.SECONDS);
     }
 }
