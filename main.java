94f18c812a88994bc6d2ac672f896ab7652adba9
A8E1 <98847922+A8E1@users.noreply.github.com>
2024-09-06 16:19:37 -0500

    added a new sound for focus & break times
    added luffy soundbite for focus time, and changed the naruto soundbite for break times. I also changed the method used on executorService to fit the anime-doro functionality.

--- main.java
+++ main.java
@@ -7,13 +7,38 @@ import java.util.concurrent.TimeUnit;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineEvent;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class main {
 
-    static Runnable print = new Runnable() {
+    static Runnable focusTime = new Runnable() {
+        public void run() {
+            try {
+                // Specify the path to the wav file
+                File audioFile = new File("luffy.wav");
+
+                // Create an AudioInputStream from the file
+                AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+
+                // Get an audio clip resource
+                Clip audioClip = AudioSystem.getClip();
+
+                // Open the audio clip with the audio stream
+                audioClip.open(audioStream);
+
+                // Play the audio clip
+                audioClip.start();
+
+                System.out.println("Focus time!");
+
+            } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    static Runnable breakTime = new Runnable() {
         public void run() {
             try {
                 // Specify the path to the wav file
@@ -30,6 +55,7 @@ public class main {
 
                 // Play the audio clip
                 audioClip.start();
+                System.out.println("Anime episode time!");
 
             } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
                 e.printStackTrace();
@@ -40,6 +66,8 @@ public class main {
     public static void main(String[] args) {
         ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
 
-        executorService.scheduleAtFixedRate(print, 0, 3, TimeUnit.SECONDS);
+        executorService.schedule(focusTime, 2, TimeUnit.SECONDS);
+        executorService.schedule(breakTime, 25, TimeUnit.SECONDS);
+
     }
 }
