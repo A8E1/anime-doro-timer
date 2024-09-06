863a9d98d171d126528b891b33cec64064371f5c
A8E1 <98847922+A8E1@users.noreply.github.com>
2024-09-06 15:43:02 -0500

    added sound functionality
    ive added the clip, inputstream, and exceptions handing libraries in order to play a specific soundbite at the end of my timer

--- main.java
+++ main.java
@@ -1,12 +1,39 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class main {
 
     static Runnable print = new Runnable() {
         public void run() {
-            System.out.println("hello");
+            try {
+                // Specify the path to the wav file
+                File audioFile = new File("naruto.wav");
+
+                // Create an AudioInputStream from the file
+                AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+
+                // Get an audio clip resource
+                Clip audioClip = AudioSystem.getClip();
+
+                // Open the audio clip with the audio stream
+                audioClip.open(audioStream);
+
+                // Play the audio clip
+                audioClip.start();
+
+            } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+                e.printStackTrace();
+            }
         }
     };
 
